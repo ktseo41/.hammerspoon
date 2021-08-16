5dd110dcb0ae7d3d76f1da3888e1f8dd94920c07
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-08-16 11:55:12 +0900

    fix: escape를 누르면 영어로 전환하는 기능이 동작하도록 한다
    -  오른쪽 command로 한영 전환을 할 수 있도록 한 뒤 제대로 작동하지 않았음

--- init.lua
+++ init.lua
@@ -1,55 +1,28 @@
-local frontmostApplication = hs.application.frontmostApplication()
-
-if frontmostApplication and frontmostApplication:name() == "Code" then
-    local caps_mode = hs.hotkey.modal.new()
-    local inputEnglish = "com.apple.keylayout.ABC"
-
-    local on_caps_mode = function()
-        caps_mode:enter()
-    end
-
-    local off_caps_mode = function()
-
-        caps_mode:exit()
-
-        local input_source = hs.keycodes.currentSourceID()
-
-        if not (input_source == inputEnglish) then
-            hs.keycodes.currentSourceID(inputEnglish)
+-- if escape is pressed on vscode, then switch to English
+function detectEscapeOnCodeAndChangeInputToEnglish()
+    local frontmostApplication = hs.application.frontmostApplication()
+    if frontmostApplication and frontmostApplication:name() == "Code" then
+        local caps_mode = hs.hotkey.modal.new()
+        local inputEnglish = "com.apple.keylayout.ABC"
+        local on_caps_mode = function()
+            caps_mode:enter()
         end
-        hs.eventtap.keyStroke({'control'}, 'c')
+        local off_caps_mode = function()
+            caps_mode:exit()
+            local input_source = hs.keycodes.currentSourceID()
+            if not (input_source == inputEnglish) then
+                hs.keycodes.currentSourceID(inputEnglish)
+            end
+            hs.eventtap.keyStroke({'control'}, 'c')
+        end
+        hs.hotkey.bind({}, 'escape', on_caps_mode, off_caps_mode)
     end
-
-    hs.hotkey.bind({}, 'escape', on_caps_mode, off_caps_mode)
 end
-
--- -- https://johngrib.github.io/wiki/hammerspoon-tutorial-03/
--- do  -- input sorce changer
---     local inputSource = {
---         english = "com.apple.keylayout.ABC",
---         korean = "com.apple.inputmethod.Korean.2SetKorean",
---     }
-
---     local changeInput = function()
-
---         local current = hs.keycodes.currentSourceID()
---         local nextInput = nil
-
---         if current == inputSource.english then
---             nextInput = inputSource.korean
---             hs.alert("ㄱㄴㄷ")
---         else
---             nextInput = inputSource.english
---             hs.alert("ABC")
---         end
---         hs.keycodes.currentSourceID(nextInput)
---     end
-
---     hs.hotkey.bind({'shift'}, 'space', changeInput)
--- end
 
 -- https://github.com/Hammerspoon/hammerspoon/issues/1039
 layoutWatcher = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(e)
+    detectEscapeOnCodeAndChangeInputToEnglish()
+
     local flags = e:getFlags()
     local inputSource = {
         english = "com.apple.keylayout.ABC",
