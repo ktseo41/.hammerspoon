fc8ef97fa0035d01eb0d64af0f0e8a552459ab8d
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-04-28 03:12:47 -0700

    터미널을 열 수 있도록 한다

--- init.lua
+++ init.lua
@@ -1,29 +1,3 @@
--- -- if escape is pressed on vscode, then switch to English
--- function detectEscapeOnCodeAndChangeInputToEnglish()
---     local frontmostApplication = hs.application.frontmostApplication()
---     if frontmostApplication and frontmostApplication:name() == "Code" then
---         local caps_mode = hs.hotkey.modal.new()
---         local inputEnglish = "com.apple.keylayout.ABC"
---         local on_caps_mode = function()
---             caps_mode:enter()
---         end
---         local off_caps_mode = function()
---             caps_mode:exit()
---             local input_source = hs.keycodes.currentSourceID()
---             if not (input_source == inputEnglish) then
---                 hs.keycodes.currentSourceID(inputEnglish)
---             end
---             hs.eventtap.keyStroke({'control'}, 'c')
---         end
---         hs.hotkey.bind({}, 'escape', on_caps_mode, off_caps_mode)
---     else
---       hs.eventtap.keyStroke({}, 'escape')
---     end
--- end
-
--- -- init detectEscapeOnCodeAndChangeInputToEnglish
--- detectEscapeOnCodeAndChangeInputToEnglish()
-
 do
   hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
     hs.reload()
@@ -52,5 +26,11 @@ end
 do
   hs.hotkey.bind({"cmd", "alt"}, "O", function()
     hs.application.open("Visual Studio Code")
+  end)
+end
+
+do
+  hs.hotkey.bind({"cmd", "alt"}, "E", function()
+    hs.application.open("Terminal")
   end)
 end
